419b2d209c268daa1a015f79a0d539ad52847d19
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-04-12 17:19:09 -0400

    initial commit

--- frontend/covid-tracker/plugins/chartkick.js
+++ frontend/covid-tracker/plugins/chartkick.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 import Chartkick from 'vue-chartkick'
 import Chart from 'chart.js'
 
-Chartkick.configure({mapsApiKey: "AIzaSyAVoZMoCXcDhEcm5YCY_G-yxVnq8ZyjWyY"})
+Chartkick.configure({mapsApiKey: process.env.GOOGLE_MAPS_API_KEY})
 
 Vue.use(Chartkick.use(Chart))
