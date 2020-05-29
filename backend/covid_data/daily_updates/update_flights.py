cbfbe8051920fbe9cede778aa2d4de7836d94e8f
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-05-29 15:58:30 -0400

    multi thread support

--- backend/covid_data/daily_updates/update_flights.py
+++ backend/covid_data/daily_updates/update_flights.py
@@ -1,6 +1,10 @@
 import os
 import requests
+from random import random
+import time as t
+from itertools import repeat
 from datetime import datetime, date, time , timedelta
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
 import pytz
 from pytz import utc, timezone
 from covid_data.models import Airport, DailyFlights, State
@@ -10,34 +14,60 @@ username = os.environ['OPENSKY_USERNAME']
 password = os.environ['OPENSKY_PASSWORD']   
 
 def get_flights_by_region(region, flights_date):
+    
+    regional_airports = Airport.objects.filter(region=region)
     number_of_inbound_flights = 0
     number_of_outbound_flights = 0
-
-    regional_airports = Airport.objects.filter(region=region)
-
-    for airport in regional_airports:
-        begin_timestamp, end_timestamp = get_local_timestamp(airport.timezone, flights_date)
-        
-        inbound_flights_request_str = "https://" + username + ":" + password + "@opensky-network.org/api/flights/arrival?airport=" + str(airport.icao_code) + "&begin=" + str(begin_timestamp) + "&end=" + str(end_timestamp)
-        outbound_flights_request_str = "https://" + username + ":" + password + "@opensky-network.org/api/flights/departure?airport=" + str(airport.icao_code) + "&begin=" + str(begin_timestamp) + "&end=" + str(end_timestamp)
-
-        inbound_flights_json = api_request_from_str(inbound_flights_request_str)
-        outbound_flights_json = api_request_from_str(outbound_flights_request_str)
-        
-        try:
-            for flight in inbound_flights_json:
-                if flight:
-                    number_of_inbound_flights += 1
-        except:
-            print("Couldn't get inbound flights for " + str(airport.airport_name) + " on " + str(flights_date))
-
-        try:
-            for flight in outbound_flights_json:
-                if flight:
-                    number_of_outbound_flights += 1
-        except:
-            print("Couldn't get outbound flights for " + str(airport.airport_name) + " on " + str(flights_date))
     
+    with ThreadPoolExecutor(max_workers=3) as e:
+        futures = {e.submit(get_flights_by_airport, regional_airport, flights_date): regional_airport for regional_airport in regional_airports}
+    
+        for future in as_completed(futures):
+            number_of_inbound_flights += (future.result()[0])
+            number_of_outbound_flights += (future.result()[1])
+
+    return number_of_inbound_flights, number_of_outbound_flights
+
+def get_inbound_flights(inbound_flights_json):
+    number_of_inbound_flights = 0
+    try:
+        for flight in inbound_flights_json:
+            if flight:
+                number_of_inbound_flights += 1
+    except:
+        print("Couldn't get flight data")
+        #print("Couldn't get inbound flights for " + str(airport.airport_name) + " on " + str(flights_date))
+    return number_of_inbound_flights
+
+def get_outbound_flights(outbound_flights_json):
+    number_of_outbound_flights = 0
+    try:
+        for flight in outbound_flights_json:
+            if flight:
+                number_of_outbound_flights += 1
+    except:
+        print("Couldn't get flight data")
+        #print("Couldn't get outbound flights for " + str(airport.airport_name) + " on " + str(flights_date))
+    return number_of_outbound_flights
+
+def get_flights_by_airport(airport, flights_date):
+    begin_timestamp, end_timestamp = get_local_timestamp(airport.timezone, flights_date)
+
+    t.sleep(random())    
+    inbound_flights_request_str = "https://" + username + ":" + password + "@opensky-network.org/api/flights/arrival?airport=" + str(airport.icao_code) + "&begin=" + str(begin_timestamp) + "&end=" + str(end_timestamp)
+    t.sleep(random())
+    outbound_flights_request_str = "https://" + username + ":" + password + "@opensky-network.org/api/flights/departure?airport=" + str(airport.icao_code) + "&begin=" + str(begin_timestamp) + "&end=" + str(end_timestamp)
+
+    inbound_flights_json = api_request_from_str(inbound_flights_request_str)
+    outbound_flights_json = api_request_from_str(outbound_flights_request_str)
+        
+    with ProcessPoolExecutor(max_workers=2) as p:
+        inbound_flights_task = p.submit(get_inbound_flights, inbound_flights_json)
+        outbound_flights_task = p.submit(get_outbound_flights, outbound_flights_json)
+        
+    number_of_inbound_flights = inbound_flights_task.result()
+    number_of_outbound_flights = outbound_flights_task.result()
+
     return number_of_inbound_flights, number_of_outbound_flights
 
 def update_regional_flights(region, date_to_update):
