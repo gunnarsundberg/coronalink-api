f218a14c26461be17bc2ee1cc3066279a8f17fc0
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-04 16:45:36 -0400

    remove log reference

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -4,8 +4,7 @@ module.exports = {
     exec_mode : 'cluster', // enables clustering
     instances : '2', // or an integer
     //cwd       : './current', // only if using a subdirectory
-    script    : './node_modules/nuxt-start/bin/nuxt-start.js', // The magic ke
-    log_file  : '/tmp/covidfrontend.log'
+    script    : './node_modules/nuxt-start/bin/nuxt-start.js',
   }],
 
   deploy : {
