26ab1f267d60618906e3ad81d075e260594d76fa
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-04 17:05:12 -0400

    change pm2 config

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -6,17 +6,4 @@ module.exports = {
     //cwd       : './current', // only if using a subdirectory
     script    : './node_modules/nuxt-start/bin/nuxt-start.js',
   }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
 };
