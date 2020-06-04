bd2c75820e2772fd408bcdee245dc19bf08c8a3a
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-04 14:58:39 -0400

    pm2 config update

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,16 +1,10 @@
 module.exports = {
   apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }, {
     name      : 'covidfrontend', // App name that shows in `pm2 ls`
     exec_mode : 'cluster', // enables clustering
     instances : '2', // or an integer
     //cwd       : './current', // only if using a subdirectory
-    script    : './node_modules/nuxt/bin/nuxt-start', // The magic ke
+    script    : './node_modules/nuxt-start/bin/nuxt-start', // The magic ke
   }],
 
   deploy : {
