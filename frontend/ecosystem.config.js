5c36f1dd3b7ae9280fc5797e1fce451af9c2858e
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-08 12:26:42 -0400

    remove cluster mode for now

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   apps : [{
     name      : 'covidfrontend', // App name that shows in `pm2 ls`
-    exec_mode : 'cluster', // enables clustering
-    instances : '2',
+    //exec_mode : 'cluster', // enables clustering
+    instances : '1',
     //cwd       : './current', // only if using a subdirectory
     script: 'npm run build && npm run start',
     watch: true,
