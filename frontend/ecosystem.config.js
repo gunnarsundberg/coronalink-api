a23d20cb712c5ea906762ba7213aea7e4c10c16b
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-08 09:57:34 -0400

    chnage nuxt start script

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   apps : [{
     name      : 'covidfrontend', // App name that shows in `pm2 ls`
-    exec_mode : 'cluster', // enables clustering
-    instances : '2', // or an integer
+    //exec_mode : 'cluster', // enables clustering
+    //instances : '2', // or an integer
     //cwd       : './current', // only if using a subdirectory
-    script    : './node_modules/nuxt-start/bin/nuxt-start.js',
+    script: 'npm run clear && npm run build && npm run start',
+    instances: 1,
+    autorestart,
+    watch,
+    max_memory_restart: maxMemoryRestart,
+    //env: dotenv.config({ path: './config/.env.pro' }).parsed
   }],
 };
