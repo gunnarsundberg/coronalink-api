c989bdb8c84b406d0f2a00f1dadd25f77fc193de
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-08 12:12:02 -0400

    config updates

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,12 +1,10 @@
 module.exports = {
   apps : [{
     name      : 'covidfrontend', // App name that shows in `pm2 ls`
-    //exec_mode : 'cluster', // enables clustering
-    //instances : '2', // or an integer
+    exec_mode : 'cluster', // enables clustering
+    instances : '2',
     //cwd       : './current', // only if using a subdirectory
-    script: 'npm run clear && npm run build && npm run start',
-    instances: 1,
+    script: 'npm run build && npm run start',
     watch: true,
-    //env: dotenv.config({ path: './config/.env.pro' }).parsed
   }],
 };
--- frontend/mixins/helper.js
+++ frontend/mixins/helper.js
@@ -9,10 +9,16 @@ export function progressionChartData(dailyData) {
     var newChartData = []
     for (var i=dailyData.length - 1; i >= 0 ; i--) {
         var newElement = [];
-        console.log(i)
         newElement[0] = dailyData[i]['date'] 
         newElement[1] = dailyData[i]['cases']
         newChartData.push(newElement);
     }
     return newChartData
+}
+
+export function compareCases(a, b) {
+    if (a['cases'] > b['cases']) return -1;
+    if (b['cases'] > a['cases']) return 1;
+
+    return 0;
 }
