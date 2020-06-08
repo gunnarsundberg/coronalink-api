01784d873fb52eb5473f3644f24a5a9e745c0e9b
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-08 11:50:13 -0400

    update API IP

--- frontend/components/StateView.vue
+++ frontend/components/StateView.vue
@@ -128,8 +128,8 @@ export default {
 
         /* Import outbreak data for a state (given in params). This is called each time a state is selected. */
         async updateStateData (state) {
-            const stateDailyRequest = await axios.get(process.env.API_HOST + process.env.PORT + '/api/v1/outbreak/daily/states?state=' + state)
-            const stateCumulativeRequest = await axios.get(process.env.API_HOST + process.env.PORT + '/api/v1/outbreak/cumulative/historic/states?state=' + state)
+            const stateDailyRequest = await axios.get('http://161.35.60.204/api/v1/outbreak/daily/states?state=' + state)
+            const stateCumulativeRequest = await axios.get('http://161.35.60.204/api/v1/outbreak/cumulative/historic/states?state=' + state)
             
             this.stateDailyData = stateDailyRequest.data
             this.stateCumulativeData = stateCumulativeRequest.data
