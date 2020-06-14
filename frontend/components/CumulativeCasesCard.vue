1a2b91cc6c51ecf856965b613c9c5df8ae135efc
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-14 15:42:36 -0400

    separate cumulative cases card into component

---
+++ frontend/components/CumulativeCasesCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <b-card title="Cumulative Cases" class="shadow" align="center" align-v="center">
+        <b-card-text align-middle>
+            <h1>{{ cumulativeCases }}</h1>
+        </b-card-text>
+    </b-card>
+</template>
+
+<script>
+export default {
+    props: {
+        cumulativeCases: {
+            required: true
+        }
+    }
+}
+</script>
