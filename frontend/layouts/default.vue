8c21b3578d70548cab03601f70914b5debcd8cb9
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-13 21:09:56 -0400

    new sidebar nav and page headers

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,25 +1,54 @@
 <template>
-  <div>
-    <b-navbar toggleable="lg" type="light" class="my-0 py-0 px-0">
-      <b-navbar-brand class="mb-0 pb-4 pt-3" style="background-color: #532d8e;">
-        <img src="https://floridapoly.edu/wp-content/themes/floridapolytechnic/images/logo.svg" width="250" height="40" alt="">
-      </b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse" class="mx-5"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ml-auto px-5">
-          <b-nav-item href="#"><nuxt-link to="/">Home</nuxt-link></b-nav-item>
-          <b-nav-item href="#"><nuxt-link to="/data">Data</nuxt-link></b-nav-item>
-          <b-nav-item href="#"><nuxt-link to="/api">API</nuxt-link></b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    
-    </b-navbar>
-    <nuxt />
+  <div id="app">
+    <Slide :closeOnNavigation="true" class="bg-dark">
+      <a id="home">
+        <span><nuxt-link to="/">Dashboard</nuxt-link></span>
+      </a>
+      <a id="">
+        <span><nuxt-link to="/data">Data</nuxt-link></span>
+      </a>
+      <a id="">
+        <span><nuxt-link to="/api">API</nuxt-link></span>
+      </a>
+    </Slide>
+    <main id="page-wrap">
+      <div class="px-5 py-5" style="background: #654ea3; background: -webkit-linear-gradient(to right, #654ea3, #eaafc8); background: linear-gradient(to right, #654ea3, #eaafc8);">
+        <div class="my-0 py-5 px-5 text-white">
+            <h1 class="pt-5 px-5 header">FLORIDA POLY</h1>
+            <h1 class="pb-0 px-5 header">COVID PROJECT</h1>
+            <p class=" px-5 pb-5">Data and insights related to the COVID-19 pandemic</p>
+        </div>
+      </div>
+      <nuxt />
+    </main>
   </div>
 </template>
 
-
 <style>
+  .bm-burger-bars {
+      background-color: white !important;
+    }
+  .line-style {
+      position: absolute;
+      height: 10% !important;
+      left: 0;
+      right: 0;
+  }
+  .bm-item-list > * > span {
+      margin-left: 10px;
+      font-weight: 700;
+      color: white !important;
+  }
+  .bm-menu {
+    background-color: #343a40 !important;
+  } 
+  .nuxt-link {
+    color: white !important;
+  }
+  .header {
+    font-family: 'Miriam Libre', sans-serif;
+  }
+  div {  
+    font-family: 'Open Sans', sans-serif;
+  }
 </style>
