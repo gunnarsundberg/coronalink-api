baa8660201daec11f219827a441a60ee8991afd4
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-14 15:46:17 -0400

    update progression chart data for national view

--- frontend/mixins/helper.js
+++ frontend/mixins/helper.js
@@ -8,9 +8,16 @@ export function numberWithCommas(x) {
 export function progressionChartData(dailyData) {
     var newChartData = []
     for (var i=dailyData.length - 1; i >= 0 ; i--) {
-        var newElement = [];
-        newElement[0] = dailyData[i]['date'] 
-        newElement[1] = dailyData[i]['cases']
+        var newElement = [];    
+        if (dailyData[i]['cases']) {
+            newElement[0] = dailyData[i]['date']
+            newElement[1] = dailyData[i]['cases']
+        }
+        else {
+            var date = dailyData[i]['date'].toString()
+            newElement[0] = date.replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3")
+            newElement[1] = dailyData[i]['positive']
+        }
         newChartData.push(newElement);
     }
     return newChartData
@@ -21,4 +28,16 @@ export function compareCases(a, b) {
     if (b['cases'] > a['cases']) return 1;
 
     return 0;
+}
+
+export function percentChange(initialData, newData) {
+    return ((((newData - initialData)/initialData) * 100).toFixed(1))
+}
+
+export function deathIncrease(newDeaths, initialDeaths) {
+    return newDeaths - initialDeaths
+}
+
+export function caseIncrease(newCases, initialCases) {
+    return newCases - initialCases
 }
