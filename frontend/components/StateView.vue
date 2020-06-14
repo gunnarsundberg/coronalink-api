997a2206dcb06b1109abbd8ae7bfbc17717d1422
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-14 15:45:04 -0400

    remove unnecessary prop

--- frontend/components/StateView.vue
+++ frontend/components/StateView.vue
@@ -25,10 +25,6 @@ export default {
     },
 
     props: {
-        nationalOutbreak: {
-            type: Array,
-            required: true
-        },
         currentState: {
             required: true
         }
