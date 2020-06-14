076c140fc674a0852430ff1ea8ca2d3561794167
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-13 21:08:41 -0400

    remove overview text and auto import states

--- frontend/components/StateView.vue
+++ frontend/components/StateView.vue
@@ -1,30 +1,11 @@
 <template>
     <div>
-        <div class="d-flex justify-content-center pt-3" style="background: #f6f5f3;">
-        </div>
-        <div class="mx-0 bg-white">
-            <div style="background: #f6f5f3;" class="px-5 mx-0">
-                <div class="mx-0 px-5 mb-5">
-                    <div class="row justify-content-between pb-0 mb-0">
-                        <b-col>
-                            <h3 v-if="dataLoaded(stateDailyData)">Overview</h3>
-                            <h3 v-else>Select a State</h3>
-                        </b-col>
-                        <b-col class="float-right">
-                            <b-form-select v-model="currentState" :options="states"  @change="updateStateData" class="w-50 mb-3 float-right"></b-form-select>
-                        </b-col>
-                    </div>
-                    <p v-if="dataLoaded(stateDailyData)" class="text-muted pt-0">Data from {{ newestDate }}</p>
-                </div>
-
+        <div>
+            <div class="px-5 mx-0 py-5">
                 <state-dash-cards v-if="(dataLoaded(stateDailyData) && dataLoaded(stateCumulativeData))" :stateDailyData="stateDailyData" :stateCumulativeData="stateCumulativeData"></state-dash-cards>
-               
             </div>
-
             <state-fast-facts v-if="dataLoaded(stateDemographics)" :currentState="currentState" :stateDemographics="stateDemographics"></state-fast-facts>
-
-            <state-progression v-if="dataLoaded(stateDailyData)" :stateCumulativeData="stateCumulativeData" :currentState="currentState"></state-progression>
-
+            <state-progression v-if="dataLoaded(stateDailyData)" :stateCumulativeData="stateCumulativeData" :currentState="currentState" class="mx-0 bg-white"></state-progression>
         </div>
     </div>
 </template>
@@ -48,78 +29,20 @@ export default {
             type: Array,
             required: true
         },
+        currentState: {
+            required: true
+        }
     },
 
     data() {
         return {
-            states: [
-                'AL', 
-                'AK', 
-                'AZ', 
-                'AR', 
-                'CA', 
-                'CO', 
-                'CT', 
-                'DE', 
-                'DC', 
-                'FL', 
-                'GA', 
-                'HI', 
-                'ID', 
-                'IL', 
-                'IN', 
-                'IA', 
-                'KS', 
-                'KY', 
-                'LA', 
-                'ME', 
-                'MD', 
-                'MA', 
-                'MI', 
-                'MN', 
-                'MS', 
-                'MO', 
-                'MT', 
-                'NE', 
-                'NV', 
-                'NH', 
-                'NJ', 
-                'NM', 
-                'NC', 
-                'ND', 
-                'OH', 
-                'OK', 
-                'OR', 
-                'PA', 
-                'RI', 
-                'SC', 
-                'SD', 
-                'TN', 
-                'TX', 
-                'UT', 
-                'VT', 
-                'VA', 
-                'WA', 
-                'WV', 
-                'WI', 
-                'WY', 
-                'NY',
-        ],
-            currentState: 'AL',
             stateDailyData: null,
             stateCumulativeData: null,
             stateDemographics: null,
         }
     },
-
-    computed: {
-        newestDate: function () {
-            return this.stateDailyData[0]['date']
-        }
-
-    },
   
-  /* Method used by .sort() for sorting logic. Uses number of cases for sorting. */
+    /* Method used by .sort() for sorting logic. Uses number of cases for sorting. */
     methods: {
         progressionChartData,
 
@@ -137,6 +60,9 @@ export default {
             this.stateCumulativeData = stateCumulativeRequest.data
             this.stateDemographics = stateDemographics.data;
         },
+    },
+    mounted: function () {
+        this.updateStateData(this.currentState)
     }
 
 }
