1d2f12e0849b0f0e53aea73c66d02e7bb208f71f
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-13 21:10:40 -0400

    new selection interface and state imports

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="mx-0 px-0 py-5">
-    <div class="mb-5">
-      <div class="pb-5">
-          <h1 class="py-5 header" align="center">Poly COVID Tracking Project</h1>
-          <div class="res-circle mb-5" align="center"></div>
+  <div class="mx-0 my-0 px-0" style="background: #f6f5f3;">
+    <div class="px-4 pt-5">
+      <div class="px-4">
+        <h3 class="px-5">Overview</h3>
+        <div class="row justify-content-between px-5">
+          <p class="text-muted px-3">Data from {{ newestDate }}</p>
+          <v-selectmenu :data="menu" :query="true" language="en" type="advanced" key-field="code" show-field="name" align="right" class="px-3" :title="title" v-model="currentRegion"></v-selectmenu>
+        </div>
       </div>
-      <b-tabs align="center" class="pt-5">
-        <b-tab title="National View"><NationalView :nationalOutbreak="nationalOutbreak"></NationalView></b-tab>
-        <b-tab title="State View"><StateView v-if="nationalOutbreak" :nationalOutbreak="nationalOutbreak"></StateView></b-tab>
-      </b-tabs>
+    </div>
+    <div>
+      <NationalView v-if="currentRegion == 'US'" :nationalOutbreak="stateCumulative"></NationalView>
+      <StateView v-if="isState(states,currentRegion)" :nationalOutbreak="stateCumulative" :currentState="currentRegion"></StateView>
     </div>
   </div>
 </template>
@@ -25,15 +28,59 @@ export default {
   },
   data() {
     return {
-
+      currentRegion: 'US',
+      title: false,
+      menu: [
+        {
+          title: 'National',
+          list: [
+            {code: 'US', name: 'United States'}
+          ]
+        },
+        {
+          title: 'State',
+          list: []
+        }
+      ]
     }
   },
   /* Initial import for national outbreak data. Called before initial page load. */
   async asyncData () {
-      const {data} = await axios.get('http://161.35.60.204/api/v1/outbreak/cumulative/states')
-      return {nationalOutbreak:data}
+      const stateCumulative = await axios.get('http://161.35.60.204/api/v1/outbreak/cumulative/states')
+      const states = await axios.get('http://161.35.60.204/api/v1/regions/states')
+      return {stateCumulative: stateCumulative.data, states: states.data}
   },
+  
+  computed: {
+    newestDate: function () {
+      return this.stateCumulative[0]['date']
+    }
+  },
+
+  methods: {
+    getMenuStates: function (states) {
+      var newStateMenu = []
+      for (var i=0; i < states.length; i++) {
+        var newStateInstance = {
+          code: states[i]['code'],
+          name: states[i]['name']
+        }
+        newStateMenu.push(newStateInstance)
+      }
+      return this.menu[1].list = newStateMenu
+    },
+
+    isState: function(states, regionCode) {
+      let obj = states.find(o => o.code === regionCode);
+      return obj != null
+    }
+  },
+
+  mounted: function () {
+    this.getMenuStates(this.states)
+  }
 }
 </script>
 <style>
+  
 </style>
