55be98d8dbb5886a63dc7033bd527685ad4bb556
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-14 15:43:19 -0400

    separate new daily cases chart card into component

---
+++ frontend/components/NewCasesChartCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <b-card title="New Cases by Day" class="shadow">
+        <div class="card-body">
+            <column-chart :data="progressionChartData(stateDailyData)" min="0" :library="{scales: {xAxes: [{ticks: {display: false}}]}}" :label="false" height="150px"></column-chart>
+        </div>
+    </b-card>
+</template>
+
+<script>
+import {progressionChartData} from '~/mixins/helper.js'
+export default {
+    props: {
+        stateDailyData: {
+            type: Array,
+            required: true
+        }
+    },
+
+    methods: {
+        progressionChartData
+    }
+}
+</script>
