43d7c2f5866f914ef7b32508415f5d19f21c57eb
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-24 16:35:22 -0400

    add date field to list view and allow sort by date

--- backend/covid_data/admin.py
+++ backend/covid_data/admin.py
@@ -3,16 +3,16 @@ from covid_data.models import StayInPlace, SchoolClosure
 
 @admin.register(StayInPlace)
 class StayInPlaceAdmin(admin.ModelAdmin):
-    list_display = ('region', 'order')
+    list_display = ('region', 'date', 'order')
     search_fields = ['region__name']
     date_hierarchy = 'date'
     list_filter = ('order',)
-    ordering = ('region', 'order')
+    ordering = ('region', 'order', 'date')
 
 @admin.register(SchoolClosure)
 class SchoolClosureAdmin(admin.ModelAdmin):
-    list_display = ('region', 'order')
+    list_display = ('region', 'date', 'order')
     search_fields = ['region__name']
     date_hierarchy = 'date'
     list_filter = ('order',)
-    ordering = ('region', 'order')
+    ordering = ('region', 'order', 'date')
