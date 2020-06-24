aec81d8149636f3317e27c5b9d90a6f909c1afec
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-24 16:28:41 -0400

    add stay in place and school closures to django admin

--- backend/covid_data/admin.py
+++ backend/covid_data/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from covid_data.models import StayInPlace, SchoolClosure
 
-# Register your models here.
+@admin.register(StayInPlace)
+class StayInPlaceAdmin(admin.ModelAdmin):
+    list_display = ('region', 'order')
+    search_fields = ['region__name']
+    date_hierarchy = 'date'
+    list_filter = ('order',)
+    ordering = ('region', 'order')
+
+@admin.register(SchoolClosure)
+class SchoolClosureAdmin(admin.ModelAdmin):
+    list_display = ('region', 'order')
+    search_fields = ['region__name']
+    date_hierarchy = 'date'
+    list_filter = ('order',)
+    ordering = ('region', 'order')
