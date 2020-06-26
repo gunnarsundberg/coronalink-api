e68d019fda3dfb659992dcf21deaacd384defddc
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-26 19:56:42 -0400

    condense components into numericdatacard

---
+++ frontend/components/NumericDataCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <b-card :title="title" class="shadow" align="center" align-v="center">
+        <b-card-text align-middle>
+            <h1 class="py-5">{{ numericData }}</h1>
+        </b-card-text>
+        <template v-if="footerText" v-slot:footer>
+            <small class="text-muted">{{ footerText }}</small>
+        </template>
+    </b-card>
+</template>
+
+<script>
+export default {
+    props: {
+        numericData: {
+            required: true
+        },
+        title: {
+            required: true,
+            type: String
+        },
+        footerText: {
+            required: false,
+        }
+    }
+}
+</script>
