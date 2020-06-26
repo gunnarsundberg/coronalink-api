e68a049eb2882f3d0210823d273224a8dcd2509f
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-26 19:55:46 -0400

    remove google charts

--- frontend/plugins/chartkick.js
+++ frontend/plugins/chartkick.js
@@ -2,6 +2,4 @@ import Vue from 'vue'
 import Chartkick from 'vue-chartkick'
 import Chart from 'chart.js'
 
-Chartkick.configure({mapsApiKey: process.env.MAPS_API_KEY}) 
-
 Vue.use(Chartkick.use(Chart))
