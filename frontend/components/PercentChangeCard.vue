f696cf2d7f5f18e1b6a08c0d75f39e8cf2c9e27c
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-26 19:57:04 -0400

    group percent change components into one component

---
+++ frontend/components/PercentChangeCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <b-card :title="title" class="shadow" align="center">
+        <b-card-text class="">
+            <div class="py-4">
+                <h1>{{ newData }}</h1>
+                <h3 class="text-danger" v-if="displacement(newData, initialData) > 0 && isFinite(percentChange(initialData, newData))">+{{ percentChange(initialData, newData) }}%</h3>
+                <h3 class="text-success" v-if="displacement(newData, initialData) <= 0 && isFinite(percentChange(initialData, newData))">{{ percentChange(initialData, newData) }}%</h3>
+            </div>
+        </b-card-text>
+    </b-card>
+</template>
+
+<script>
+import {displacement, percentChange} from '~/mixins/helper.js'
+export default {
+    props: {
+        newData: {
+            required: true,
+        },
+
+        initialData: {
+            required: true,
+        },
+
+        title: {
+            required: true,
+            type: String
+        }
+    },
+    methods: {
+        displacement,
+        percentChange
+    }
+}
+</script>
