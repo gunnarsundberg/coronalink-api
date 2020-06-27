88b04ba2de3e69a04d37ed4ef7cb5d1b44c5c430
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-26 20:01:13 -0400

    use new overview in place of dash cards

--- frontend/components/StateView.vue
+++ frontend/components/StateView.vue
@@ -2,24 +2,23 @@
     <div>
         <div>
             <div class="px-5 mx-0 py-5">
-                <state-dash-cards v-if="(dataLoaded(stateDailyData) && dataLoaded(stateCumulativeData))" :stateDailyData="stateDailyData" :stateCumulativeData="stateCumulativeData"></state-dash-cards>
+                <state-overview v-if="(dataLoaded(stateDailyData) && dataLoaded(stateCumulativeData))" :stateDailyData="stateDailyData" :stateCumulativeData="stateCumulativeData" :currentState="currentState"></state-overview>
             </div>
             <state-fast-facts v-if="dataLoaded(stateDemographics)" :currentState="currentState" :stateDemographics="stateDemographics"></state-fast-facts>
-            <state-progression v-if="dataLoaded(stateDailyData)" :stateCumulativeData="stateCumulativeData" :currentState="currentState" class="mx-0 bg-white"></state-progression>
+            <state-progression v-if="dataLoaded(stateDailyData)" :stateCumulativeData="stateCumulativeData" :stateDailyData="stateDailyData" :currentState="currentState" class="mx-0 bg-white"></state-progression>
         </div>
     </div>
 </template>
 
 <script>
-import StateDashCards from '~/components/StateDashCards.vue'
+import StateOverview from '~/components/StateOverview.vue'
 import StateFastFacts from '~/components/StateFastFacts.vue'
 import StateProgression from '~/components/StateProgression.vue'
 import axios from 'axios'
-import {progressionChartData} from '~/mixins/helper.js'
 
 export default {
     components: {
-        StateDashCards,
+        StateOverview,
         StateFastFacts,
         StateProgression
     },
@@ -27,7 +26,7 @@ export default {
     props: {
         currentState: {
             required: true
-        }
+        },
     },
 
     data() {
@@ -38,31 +37,33 @@ export default {
         }
     },
   
-    /* Method used by .sort() for sorting logic. Uses number of cases for sorting. */
     methods: {
-        progressionChartData,
-
         dataLoaded(data) {
             return (data != null)
         },
 
         /* Import outbreak data for a state (given in params). This is called each time a state is selected. */
-        async updateStateData (state) {
-            const stateDailyRequest = await axios.get('http://161.35.60.204/api/v1/outbreak/daily/states?state=' + state)
-            const stateCumulativeRequest = await axios.get('http://161.35.60.204/api/v1/outbreak/cumulative/historic/states?state=' + state)
-            const stateDemographics = await axios.get('http://161.35.60.204/api/v1/demographics/states?state=' + state);
+        async updateStateData () {
+            const stateDailyRequest = await axios.get('http://161.35.60.204/api/v1/outbreak/daily/states?state=' + this.currentState)
+            const stateCumulativeRequest = await axios.get('http://161.35.60.204/api/v1/outbreak/cumulative/states?state=' + this.currentState)
+            const stateDemographics = await axios.get('http://161.35.60.204/api/v1/demographics/states?state=' + this.currentState)
             
             this.stateDailyData = stateDailyRequest.data
             this.stateCumulativeData = stateCumulativeRequest.data
             this.stateDemographics = stateDemographics.data;
         },
     },
-    mounted: function () {
-        this.updateStateData(this.currentState)
+    
+    created() {
+        this.updateStateData()
     },
-    updated: function () {
-        this.updateStateData(this.currentState)
-    }
+
+    watch: {
+        currentState: function () {
+            this.updateStateData()
+        }
+    },
+
 }
 
 </script>
