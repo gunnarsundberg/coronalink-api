82c61b60b0cf305911a895762bc51f01ca57df58
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-06-30 13:24:01 -0400

    menu text styling and add footer

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -3,28 +3,33 @@
     <Slide :closeOnNavigation="true">
       <a id="home">
         <nuxt-link to="/">
-          <span>Dashboard</span>
+          <span class="nav-link">Dashboard</span>
         </nuxt-link>
       </a>
       <a id="">
         <nuxt-link to="/data">
-          <span>Data</span>
+          <span class="nav-link">Data</span>
         </nuxt-link>
       </a>
       <a id="">
         <nuxt-link to="/api">
-          <span>API</span>
+          <span class="nav-link">API</span>
         </nuxt-link>
       </a>
     </Slide>
-      <div class="px-5 py-5" style="background: #654ea3; background: -webkit-linear-gradient(to right, #654ea3, #eaafc8); background: linear-gradient(to right, #654ea3, #eaafc8);">
-        <div class="my-0 py-5 px-5 text-white">
+      <div class="px-md-5 py-5" style="background: #654ea3; background: -webkit-linear-gradient(to right, #654ea3, #eaafc8); background: linear-gradient(to right, #654ea3, #eaafc8);">
+        <div class="my-0 py-5 px-lg-5 text-white">
             <h1 class="pt-5 px-5 header">FLORIDA POLY</h1>
             <h1 class="pb-0 px-5 header">COVID PROJECT</h1>
             <p class=" px-5 pb-5">Data and insights related to the COVID-19 pandemic</p>
         </div>
       </div>
       <nuxt />
+      <div class="bg-dark text-white">
+        <div class="py-5">
+          
+        </div>
+    </div>
   </div>
 </template>
 
@@ -46,8 +51,9 @@
   .bm-menu {
     background-color: #343a40 !important;
   } 
-  .nuxt-link {
+  .nav-link {
     color: white !important;
+    font-weight: bolder;
   }
   .header {
     font-family: 'Miriam Libre', sans-serif;
