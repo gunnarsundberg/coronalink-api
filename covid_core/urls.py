5051f522f941f52a1655bea54ffc485a37ab7096
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-07-10 15:00:25 -0400

    user and api url updates

--- covid_core/urls.py
+++ covid_core/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('users.urls')),
     path('', include('django_prometheus.urls')),
-    path('api/', include('covid_data.urls'))
+    path('api/', include('covid_api.urls'))
 ]
