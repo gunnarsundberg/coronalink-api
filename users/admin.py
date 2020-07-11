3bd5bba2b83121709c43fd814bff04709a6a3e8c
Gunnar Sundberg <gunnar.sundberg82@gmail.com>
2020-07-11 10:47:43 -0400

    add user admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from users.models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
