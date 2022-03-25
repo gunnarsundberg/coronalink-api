ad952ca119faaaa118292df8f5143d7000cf7c87
Gunnar Sundberg <gunnarsundberg@pm.me>
2022-03-25 14:39:09 -0400

    remove serializers for deprecated models

--- covid_api/serializers.py
+++ covid_api/serializers.py
@@ -1,84 +1,73 @@
 from rest_framework import serializers
-from rest_framework_cache.serializers import CachedSerializerMixin
-from rest_framework_cache.registry import cache_registry
-from covid_data.models import State, Outbreak, OutbreakCumulative, DailyFlights, Demographics, DailyWeather
+from covid_api.models import State, County, RegionalCases, RegionalTests
 
-class StateSerializer(CachedSerializerMixin):
+class StateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = State
-        fields = ['name', 'code', 'fips_code']
+        fields = ['name', 'code', 'fips_code', 'land_area']
 
-class StateOutbreakSerializer(CachedSerializerMixin):
-    state = serializers.StringRelatedField(source='region.state') #StateSerializer(source='region.state')
-
-    class Meta:
-        model = Outbreak
-        fields = ['state', 'date', 'cases', 'negative_tests', 'total_tested', 'deaths', 'admitted_to_hospital', 'hospitalized', 'in_icu', 'days_since_outbreak']
-
-class StateOutbreakCumulativeSerializer(CachedSerializerMixin):
-    state = serializers.StringRelatedField(source='region.state')
-
-    class Meta:
-        model = OutbreakCumulative
-        fields = ['state', 'date', 'cases', 'negative_tests', 'total_tested', 'deaths', 'hospitalized', 'in_icu', 'days_since_outbreak', 'date_of_outbreak']
-
-"""
-class StateStayInPlaceSerializer(CachedSerializerMixin):
-    state = serializers.StringRelatedField(source='region.state')
-    order = serializers.CharField(source='get_order_display')
-
-    class Meta:
-        model = StayInPlace
-        fields = ['state', 'order', 'date']
-
-class StateSchoolClosureSerializer(CachedSerializerMixin):
-    state = serializers.StringRelatedField(source='region.state')
-    order = serializers.CharField(source='get_order_display')
+class CountySerializer(serializers.ModelSerializer):
+    state = serializers.StringRelatedField(source='parent_region.code')
     
-    class Meta:
-        model = SchoolClosure
-        fields = ['state', 'order', 'date']
-"""
+    def create(self, validated_data):
 
-class StateDailyFlightsSerializer(CachedSerializerMixin):
+        state_code = validated_data.pop('state')
+        state = State.objects.get(code=state_code)
+        parent_region = state
+        county = County.objects.create(parent_region=parent_region, **validated_data)
+        county.save()
+
+        return county
+
+    class Meta:
+        model = County
+        fields = ['name', 'state', 'fips_code', 'latitude', 'longitude', 'land_area']
+
+class StateCasesSerializer(serializers.ModelSerializer):
     state = serializers.StringRelatedField(source='region.state')
 
     class Meta:
-        model = DailyFlights
-        fields = ['state', 'date', 'number_of_inbound_flights', 'number_of_outbound_flights']
+        model = RegionalCases
+        fields = [
+            'state',
+            'date',
+            'cumulative_cases',
+            'new_cases',
+            'cumulative_deaths',
+            'new_deaths',
+        ]
 
-class StateDemographicsSerializer(CachedSerializerMixin):
+class CountyCasesSerializer(serializers.ModelSerializer):
+    state = serializers.StringRelatedField(source='region.county.parent_region.code')
+    fips_code = serializers.StringRelatedField(source='region.county.fips_code')
+    name = serializers.StringRelatedField(source='region.name')
+
+    class Meta:
+        model = RegionalCases
+        fields = [
+            'name',
+            'fips_code',
+            'state',
+            'date',
+            'cumulative_cases',
+            'new_cases',
+            'cumulative_deaths',
+            'new_deaths',
+        ]
+
+class StateTestsSerializer(serializers.ModelSerializer):
     state = serializers.StringRelatedField(source='region.state')
 
     class Meta:
-        model = Demographics
-        fields = ['state', 'population', 'population_density', 'percent_male', 'percent_female', 'median_age', 'percent_60s', 'percent_70s', 'percent_80_plus']
-
-class CountyDemographicsSerializer(CachedSerializerMixin):
-    state = serializers.StringRelatedField(source='region.county.parent_region')
-    county = serializers.StringRelatedField(source='region.county')
-
-    class Meta:
-        model = Demographics
-        fields = ['state', 'county', 'population', 'population_density', 'percent_male', 'percent_female', 'median_age', 'percent_60s', 'percent_70s', 'percent_80_plus']
-
-class StateDailyWeatherSerializer(CachedSerializerMixin):
-    state = serializers.StringRelatedField(source='region.state')
-
-    class Meta:
-        model = DailyWeather
-        fields = ['state', 'date', 'avg_temperature', 'max_temperature', 'min_temperature', 'avg_humidity', 'uv_index']
-
-class CountyDailyWeatherSerializer(CachedSerializerMixin):
-    state = serializers.StringRelatedField(source='region.county.parent_region')
-    county = serializers.StringRelatedField(source='region.county')
-
-    class Meta:
-        model = DailyWeather
-        fields = ['state', 'county', 'date', 'avg_temperature', 'max_temperature', 'min_temperature', 'avg_humidity', 'uv_index']
-
-# TODO: Weather serializer
-# TODO: Demographics serializer
-# TODO: Update all state and county serializers for new regions models
-# Future release: County Serializer
+        model = RegionalTests
+        fields = [
+            'state',
+            'date',
+            'cumulative_positive',
+            'cumulative_negative',
+            'cumulative_inconclusive',
+            'new_positive',
+            'new_negative',
+            'new_inconclusive',
+        ]
