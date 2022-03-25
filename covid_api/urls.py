29f80beb667defc0e63f09b084f2d1841ba26521
Gunnar Sundberg <gunnarsundberg@pm.me>
2022-03-25 14:40:35 -0400

    remove deprecated model endpoints

--- covid_api/urls.py
+++ covid_api/urls.py
@@ -1,32 +1,14 @@
-from django.urls import path
+from django.urls import re_path
 from rest_framework import routers
-from django.conf.urls import url
-from covid_api.views import StateOutbreakView, StateOutbreakCumulativeView, StateOutbreakCumulativeHistoricView, StateDailyFlightsView, StateDemographicsView, CountyDemographicsView, StateDailyWeatherView, CountyDailyWeatherView, StateView
-from rest_framework_cache.registry import cache_registry
-
-cache_registry.autodiscover()
+from covid_api.views import StateCasesView, CountyCasesView, StateTestsView, StateView, CountyView
 
 urlpatterns = [
-    # TODO: States URL, Demographics URL, Medical URL
-    url(r'v1/outbreak/daily/states', StateOutbreakView.as_view()),
-    url(r'v1/outbreak/cumulative/states', StateOutbreakCumulativeView.as_view()),
-    url(r'v1/outbreak/cumulative/historic/states', StateOutbreakCumulativeHistoricView.as_view()),
-    
-    # Distancing
-    #url(r'v1/distancing/stayinplace/states', StateStayInPlaceView.as_view()),
-    #url(r'v1/distancing/schoolclosure/states', StateSchoolClosureView.as_view()),
+    re_path(r'v1/cases/states', StateCasesView.as_view()),
+    re_path(r'v1/cases/counties', CountyCasesView.as_view()),
 
-    # Flights
-    url(r'v1/flights/daily/states', StateDailyFlightsView.as_view()),
-
-    # Weather
-    url(r'v1/weather/daily/states', StateDailyWeatherView.as_view()),
-    #url(r'v1/weather/daily/counties', CountyDailyWeatherView.as_view()),
-
-    # Demographics
-    url(r'v1/demographics/states', StateDemographicsView.as_view()),
-    #url(r'v1/demographics/counties', CountyDemographicsView.as_view()),
+    re_path(r'v1/tests/states', StateTestsView.as_view()),
 
     # Regions
-    url(r'v1/regions/states', StateView.as_view()),
+    re_path(r'v1/regions/states', StateView.as_view()),
+    re_path(r'v1/regions/counties', CountyView.as_view())
 ]
